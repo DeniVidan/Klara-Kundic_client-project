b1e148f2c219f925311c384d8c0288a4154670a1
Deni Vidan <denividan@gmail.com>
2024-11-04 16:57:08 +0100

    firebase init

---
+++ frontend/src/firebase/init.js
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore, collection } from 'firebase/firestore'
+import { onAuthStateChanged } from "firebase/auth";
+
+
+export const firebaseApp = initializeApp({
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
+})
+
+export const auth = getAuth(firebaseApp)
+
+export function getCurrentUser() {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
+  });
+};
+
+
